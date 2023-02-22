79c12b3b94279a797a23daaaac093ab2ed1fb42d
Christopher Grande <chrisgrande@users.noreply.github.com>
2023-02-22 01:47:49 +0000

    More testings

--- prose/models.py
+++ prose/models.py
@@ -6,6 +6,8 @@ from django.utils.html import strip_tags
 
 from prose.fields import DocumentContentField
 
+import filetype
+
 
 class AbstractDocument(models.Model):
     content = DocumentContentField()
@@ -38,24 +40,36 @@ def upload_to(instance, filename):
     return os.path.join("attachments", folder_name, filename)
 
 
+class AttachmentManager(models.Manager):
+    def create(self, with_file, for_document):
+        # Create the attachment
+        attachment = super().create(
+            file=with_file,
+            byte_size=with_file.size,
+            filename=with_file.name,
+            content_type=filetype.guess(with_file).mime,
+            Document=for_document,
+        )
+        attachment.save()
+        # Return the attachment
+        return attachment
+
+
 class Attachment(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    file = models.FileField(upload_to=upload_to)
+    file = models.FileField(upload_to=upload_to, editable=False)
     filename = models.CharField(max_length=120)
     content_type = models.CharField(max_length=120)
     byte_size = models.PositiveIntegerField()
-    metadata = models.TextField()
-    Document = models.ForeignKey(
-        "Document", on_delete=models.CASCADE, related_name="attachments"
-    )
-
-    # Save orignal filename, content type, and byte size
-    def save(self, *args, **kwargs):
-        self.filename = self.file.name
-        super().save(*args, **kwargs)
+    # metadata = models.TextField()
+    # Document = models.ForeignKey(
+    #     "Document", to_field="key", on_delete=models.CASCADE, related_name="attachments"
+    # )
 
     def __str__(self):
         return self.filename
+
+    objects = AttachmentManager()
 
 
 class Document(AbstractDocument):
--- prose/views.py
+++ prose/views.py
@@ -1,10 +1,8 @@
 from django.core.files.storage import default_storage
 from django.http import JsonResponse
 
-from uuid import uuid4
-from datetime import datetime
-
 from django.conf import settings
+from prose.models import Attachment
 
 import filetype
 
@@ -31,12 +29,12 @@ def validate_file(file):
 
 def upload_attachment(request):
     if request.method == "POST":
-        attachment = request.FILES["file"]
-        if not validate_file(attachment):
+        file = request.FILES["file"]
+        if not validate_file(file):
             return JsonResponse({"error": "File type or size not allowed"}, status=400)
-        key = f"{datetime.now().strftime('%Y/%m/%d')}/{uuid4()}.{attachment.name.split('.')[-1]}"
-        path = f"prose/{key}"
-        default_storage.save(path, attachment)
+        # Create Attachment object
+        attachment = Attachment.objects.create(with_file=file)
+        path = attachment.file.name
         payload = {
             "url": default_storage.url(path),
         }
